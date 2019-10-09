08faa0a1b5797fd6ed089990d4af8ecc25660d93
Théo Remon <theo.remon@uswery.eu>
2019-10-09 16:24:32 +0200

    Commentaires

--- view.h
+++ view.h
@@ -14,43 +14,44 @@
  */
 class View
 {
-    private:
-        /** The main window of the application. */
-        MainWindow* mainWindow;
+	private:
+		/** The main window of the application. */
+		MainWindow* mainWindow;
 
-        /** ??? */
-        ShiftMaxWindow* shiftMaxWindow ;
+		/** ??? */
+		ShiftMaxWindow* shiftMaxWindow ;
 
-        /** A window which display the area of the video which match the color selected using the Qt component. */
-        ObjectHistogramWindow* objectHistogramWindow;
+		/** A window which display the area of the video which match the color selected using the Qt component. */
+		ObjectHistogramWindow* objectHistogramWindow;
 
-        /** ??? */
-        ThresholdVideoWindow* thresholdVideoWindow;
+		/** ??? */
+		ThresholdVideoWindow* thresholdVideoWindow;
 
-        /** A Qt component allowing the selection of a color which will be used by the application to track the movements of the lamb. */
-        QColorDialog* qColorDialog;
+		/** A Qt component allowing the selection of a color which will be used by the application to track the movements of the lamb. */
+		QColorDialog* qColorDialog;
 
-        /** Export a part of the video as a video itself. */
-        ExtractVideoWindow* extractVideoWindow;
+		/** Export a part of the video as a video itself. */
+		ExtractVideoWindow* extractVideoWindow;
 
-        std::vector<QWidget*> views;
+		/** ??? */
+		std::vector<QWidget*> views;
 
-    public:
-        /**
-         * The constructor by default of the View class.
-         */
-        View();
+	public:
+		/**
+		 * The constructor by default of the View class.
+		 */
+		View();
 
-        /**
-         * The destructor of the View class.
-         */
-        ~View();
-        QWidget* getMainWindow();
-        QWidget* getShiftMaxWindow();
-        QWidget* getObjectHistogramWindow();
-        QWidget* getThresholdVideoWindow();
-        QWidget* getQColorDialog();
-        QWidget* getExtractVideoWindow();
+		/**
+		 * The destructor of the View class.
+		 */
+		~View();
+		QWidget* getMainWindow();
+		QWidget* getShiftMaxWindow();
+		QWidget* getObjectHistogramWindow();
+		QWidget* getThresholdVideoWindow();
+		QWidget* getQColorDialog();
+		QWidget* getExtractVideoWindow();
 };
 
 #endif // VIEW_H
