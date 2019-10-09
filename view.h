8831800d56b3506491a2d479c0d2d05c820a6e82
Théo Remon <theo.remon@uswery.eu>
2019-10-09 15:54:55 +0200

    Commentaires

--- view.h
+++ view.h
@@ -9,26 +9,48 @@
 #include "thresholdvideowindow.h"
 #include "extractvideowindow.h"
 
-class View{
+/**
+ * The class which matches the View part of the MVC pattern.
+ */
+class View
+{
     private:
-        MainWindow* mainWindow ;
-        ShiftMaxWindow* shiftMaxWindow ;
-        ObjectHistogramWindow* objectHistogramWindow ;
-        ThresholdVideoWindow* thresholdVideoWindow ;
-        QColorDialog* qColorDialog ;
-        ExtractVideoWindow* extractVideoWindow ;
+        /** The main window of the application. */
+        MainWindow* mainWindow;
 
-        std::vector<QWidget*> views ;
+        /** ??? */
+        ShiftMaxWindow* shiftMaxWindow ;
+
+        /** A window which display the area of the video which match the color selected using the Qt component. */
+        ObjectHistogramWindow* objectHistogramWindow;
+
+        /** ??? */
+        ThresholdVideoWindow* thresholdVideoWindow;
+
+        /** A Qt component allowing the selection of a color which will be used by the application to track the movements of the lamb. */
+        QColorDialog* qColorDialog;
+
+        /** Export a part of the video as a video itself. */
+        ExtractVideoWindow* extractVideoWindow;
+
+        std::vector<QWidget*> views;
 
     public:
+        /**
+         * The constructor by default of the View class.
+         */
         View();
-        ~View() ;
-        QWidget* getMainWindow() ;
-        QWidget* getShiftMaxWindow() ;
-        QWidget* getObjectHistogramWindow() ;
-        QWidget* getThresholdVideoWindow() ;
-        QWidget* getQColorDialog() ;
-        QWidget* getExtractVideoWindow() ;
+
+        /**
+         * The destructor of the View class.
+         */
+        ~View();
+        QWidget* getMainWindow();
+        QWidget* getShiftMaxWindow();
+        QWidget* getObjectHistogramWindow();
+        QWidget* getThresholdVideoWindow();
+        QWidget* getQColorDialog();
+        QWidget* getExtractVideoWindow();
 };
 
 #endif // VIEW_H
