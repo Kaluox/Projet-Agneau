a25a1ee715c520d7fbf63c351c43a50972c52114
Théo Remon <theo.remon@uswery.eu>
2019-10-18 14:32:52 +0200

    Comments

--- soundwave.h
+++ soundwave.h
@@ -1,30 +1,52 @@
 #ifndef SOUNDWAVE_H
 #define SOUNDWAVE_H
 
-#include "qcustomplot.h"
 #include <QAudioBuffer>
+#include "qcustomplot.h"
 
 class QAudioDecoder;
 
 class Soundwave : public QCustomPlot
 {
-      Q_OBJECT
-public:
-    Soundwave(QWidget *parent = Q_NULLPTR);
-    void setSource(const QString &fileName);
-    ~Soundwave();
-    double getPeakValue(const QAudioFormat& format);
-    QAudioDecoder* getDecoder();
+	Q_OBJECT
 
-public slots:
-    void setBuffer();
-    void plot();
+	/** Is it necessary to set the scope of the variables to public? */
+	public:
+		/** The Qt component which allow decoding the sound file. */
+		QAudioDecoder* decoder;
 
-public:
-    QAudioDecoder *decoder;
-    QAudioBuffer buffer;
-    QVector<double> samples;
-    QCPGraph *wavePlot;
+		QAudioBuffer buffer;
+		QVector<double> samples;
+		QCPGraph* wavePlot;
+
+	public:
+		/**
+		 * The constructor by default of the Soundwave class.
+		 */
+		Soundwave(QWidget* parent = Q_NULLPTR);
+
+		/**
+		 * The destructor of the Soundwave class.
+		 */
+		~Soundwave();
+
+		/**
+		 * A method which gives the path to the sound file to the QAudioDecoder object.
+		 * @param fileName The path to the sound file.
+		 */
+		void setSource(const QString& fileName);
+
+		double getPeakValue(const QAudioFormat& format);
+
+		/**
+		 * A method which returns the QAudioDecoder object.
+		 * @return The QAudioDecoder object.
+		 */
+		QAudioDecoder* getDecoder();
+
+	public slots:
+		void setBuffer();
+		void plot();
 };
 
 #endif // SOUNDWAVE_H
