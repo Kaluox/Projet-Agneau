be80a78cabe5fcab088fe41e93d63426ad555775
Kaluox <alexis-marlin@orange.fr>
2019-10-23 16:52:08 +0200

    ExtractVideoWindow commented

--- extractvideowindow.cpp
+++ extractvideowindow.cpp
@@ -1,5 +1,10 @@
 #include "extractvideowindow.h"
 
+/**
+ * This class is used to create the popup window asking the confirmation on the extraction of the video.
+ */
+
+/// Constructor of ExtractVideoWindow
 ExtractVideoWindow::ExtractVideoWindow(){
     qLabel = new QLabel ;
     qPushButton = new QPushButton("Confirmer") ;
@@ -7,6 +12,8 @@ ExtractVideoWindow::ExtractVideoWindow(){
     init() ;
 }
 
+
+/// Initalize the window with a push button and a label
 void ExtractVideoWindow::init(){
     setModal(true);
     qVBoxLayout->addWidget(qLabel);
@@ -15,9 +22,16 @@ void ExtractVideoWindow::init(){
     setLayout(qVBoxLayout);
 }
 
+
+///Set the text of the label given a QString
 void ExtractVideoWindow::setQLabel(QString q){qLabel->setText(q);}
+
+
+///Get the instance of the push button
 QPushButton* ExtractVideoWindow::getQPushButton(){return qPushButton ;}
 
+
+///Deletes the label and the push button
 ExtractVideoWindow::~ExtractVideoWindow(){
     delete qLabel ;
     delete qPushButton ;
--- extractvideowindow.h
+++ extractvideowindow.h
@@ -8,14 +8,41 @@
 
 class ExtractVideoWindow : public QDialog{
     private:
+
+        ///Layout of the window
         QVBoxLayout* qVBoxLayout ;
+
+        ///Label, ie what is written in the window
         QLabel* qLabel ;
+
+        ///Button asking for a confirmation
         QPushButton* qPushButton ;
+
+        /**
+         *@brief initializes the window
+         */
         void init() ;
     public:
+
+        /**
+         * @param q : QString
+         * @brief Sets the text on a label shown in the window
+         */
         void setQLabel(QString q) ;
+
+        /**
+         * @return instance of the push button
+         */
         QPushButton* getQPushButton() ;
+
+        /**
+         *@brief Default constructor, initializes the window with basic informations
+         */
         ExtractVideoWindow();
+
+        /**
+         *@brief Destructor, does nothing
+         */
         ~ExtractVideoWindow() ;
 };
 
