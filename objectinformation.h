9761c6df0d67ea06b1e80dfb426be894e6f91c13
Théo Remon <theo.remon@uswery.eu>
2019-11-12 14:20:35 +0100

    Comments

--- objectinformation.h
+++ objectinformation.h
@@ -5,21 +5,34 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 
-using namespace std ;
+class ObjectInformation
+{
+	private:
+		std::vector<std::vector<cv::Point>> contours;
+		cv::Point2f center;
+		float radius;
 
-class ObjectInformation{
-    public:
-        ObjectInformation();
-        void setContours(vector<vector<cv::Point> >);
-        void setCenter(cv::Point2f);
-        void setRadius(float);
-        vector<vector<cv::Point> > getContours();
-        cv::Point2f getCenter();
-        float getRadius();
-    private:
-        vector<vector<cv::Point> > contours;
-        cv::Point2f center;
-        float radius;
+	public:
+		/**
+		 * The constructor by default of the ObjectInformation class.
+		 */
+		ObjectInformation();
+
+		/**
+		 * A method which returns the list of lists of OpenCV Point objects.
+		 * @return The list of lists of OpenCV Point objects.
+		 */
+		std::vector<std::vector<cv::Point>> getContours();
+
+		/**
+		 * A method which returns the OpenCV Point that corresponds to the center.
+		 * @return The OpenCV Point that corresponds to the center.
+		 */
+		cv::Point2f getCenter();
+		float getRadius();
+		void setContours(vector<vector<cv::Point>>);
+		void setCenter(cv::Point2f);
+		void setRadius(float);
 };
 
 #endif // OBJECTINFORMATION_H
